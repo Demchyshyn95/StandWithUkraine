49ce063cbda973248491463dcfdb7f8cbc3bd80d
Andre <demchyshyn95@gmail.com>
2020-11-20 13:05:38 +0200

    update files

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Button, Input, Container } from 'reactstrap';
+import './style.css'
 
 const Search = ({getWeather}) => {
     return (
         <form onSubmit={getWeather}>
             <Input type="text" name="city" placeholder="Enter your city..." className="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm"/>
-            <Button color="success">Get the weather</Button>
+            <button  className=' asd btn-success btn-lg'>Search</button>
         </form>
     )
 }
